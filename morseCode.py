28865aa0db2d8abde8ac2f3f2999b6471eae5d9c
Tina1409 <112124318+Tina1409@users.noreply.github.com>
2023-10-22 08:27:00 +0530

    Add files via upload

--- morseCode.py
+++ morseCode.py
@@ -1,11 +1,9 @@
-#importing necessary libraries
-import RPi.GPIO as GPIO 
-import tkinter as tk  
-from tkinter import ttk  
-import time  
+import RPi.GPIO as GPIO
+import tkinter as tk
+from tkinter import ttk, messagebox
+import time
 
-#creating a cutsom morse code
-morse_code_dic = {
+morse_code_dict = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
     'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
@@ -13,60 +11,64 @@ morse_code_dic = {
     'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
     'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
     '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
-    '9': '----.', ' ': '/'  
+    '9': '----.', ' ': '/'
 }
 
-# Setting up the GPIO pins
-led_pin = 7 
-GPIO.setmode(GPIO.BOARD)  
-GPIO.setup(led_pin, GPIO.OUT)  
+LED_PIN = 10
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-#function for led to blink corresponding to the morse code generated
-def custom_blink_morse_code(text):
-    for char in text.upper():
-        if char in morse_code_dic:
-            morse_code = morse_code_dic[char]
-            for character in morse_code:
-                if character == '.':
-                    GPIO.output(led_pin, GPIO.HIGH)
-                    time.sleep(0.2)  
-                    GPIO.output(led_pin, GPIO.LOW)
-                    time.sleep(0.2)  
-                elif character == '-':
-                    GPIO.output(led_pin, GPIO.HIGH)
-                    time.sleep(0.6)  
-                    GPIO.output(led_pin, GPIO.LOW)
-                    time.sleep(0.2)  
-            time.sleep(0.4)  
+def blink_morse_code(name):
+    name = name.upper()
+    for char in name:
+        if char in morse_code_dict:
+            morse_code = morse_code_dict[char]
+            for symbol in morse_code:
+                if symbol == '.':
+                    GPIO.output(LED_PIN, GPIO.HIGH)
+                    time.sleep(0.2)
+                    GPIO.output(LED_PIN, GPIO.LOW)
+                    time.sleep(0.2)
+                elif symbol == '-':
+                    GPIO.output(LED_PIN, GPIO.HIGH)
+                    time.sleep(0.6)
+                    GPIO.output(LED_PIN, GPIO.LOW)
+                    time.sleep(0.2)
+            time.sleep(0.4)
         else:
-            #handling unknown characters with a long time delay
-            time.sleep(0.8)  
+            time.sleep(0.8)
 
-# setting up the GUI
-root = tk.Tk()  
-root.title("Morse Code Led Blinker")  #title of the window
+def start_blinking():
+    name = entry.get()
+    if len(name) <= 12:
+        blink_morse_code(name)
+    else:
+        messagebox.showerror("Error", "You cannot enter more than 12 letters")
 
-#handling operations when button is clicked
-def blinking():
-    text = custom_entry.get()  
-    custom_blink_morse_code(text)  
+root = tk.Tk()
+root.title("Name to Morse Code LED Blinker")
 
-#GUI background
-custom_frame = ttk.Frame(root, padding=(20, 20))
-custom_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-custom_frame.columnconfigure(0, weight=1)
-custom_frame.rowconfigure(0, weight=1)
+frame = ttk.Frame(root, padding=(20, 20), style="My.TFrame")
+frame.grid(column=0, row=1, sticky=(tk.W, tk.E, tk.N, tk.S))
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
 
-#creating a label
-custom_label = ttk.Label(custom_frame, text="Enter your name", font=("Arial", 20), foreground="blue")
-custom_label.grid(column=0, row=0, columnspan=2, pady=(0, 20))
+name_label = ttk.Label(root, text="Tina", font=("Courier", 20), foreground="pink")
+name_label.grid(column=0, row=0, columnspan=2, pady=(20, 10))
 
-#text input
-custom_entry = ttk.Entry(custom_frame, width=20, font=("Arial", 20), background="lightblue")
-custom_entry.grid(column=0, row=1, columnspan=2, pady=(0, 10))
+entry = ttk.Entry(frame, width=20, font=("Courier", 20), background="pink")
+entry.grid(column=0, row=0, columnspan=2, pady=(0, 20))
 
-custom_button = ttk.Button(custom_frame, text="Blink", command=blinking)
-custom_button.grid(column=0, row=2, columnspan=2, pady=(0, 20))
+button = ttk.Button(frame, text="Start  ", command=start_blinking, style="TButton")
+button.grid(column=0, row=1, columnspan=2, pady=(0, 20))
+
+style = ttk.Style()
+style.configure("TButton", background="pink", foreground="white", font=("Times New Roman", 20))
+style.configure("My.TFrame", background="pink")
+
+# Revert the frame size to normal
+frame.config(padding=(20, 20))
 
 root.mainloop()
+
 GPIO.cleanup()
